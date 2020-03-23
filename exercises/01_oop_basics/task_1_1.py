24cc2ede5e357b3463b57ebc055175ee4e590e3c
fortredux <fortunaredux@protonmail.com>
2020-03-23 22:44:46 +0000

    Done task_1_1 from chapter 01

--- exercises/01_oop_basics/task_1_1.py
+++ exercises/01_oop_basics/task_1_1.py
@@ -64,3 +64,52 @@ In [17]: net1.unassigned()
 Out[17]: ('10.1.1.1', '10.1.1.2', '10.1.1.3', '10.1.1.5')
 
 '''
+
+
+import ipaddress
+
+
+class IPv4Network:
+    def __init__(self, network):
+        self.network = network
+        subnet = ipaddress.ip_network(self.network)
+        self._hosts = [str(host) for host in subnet.hosts()]
+        self.address = str(subnet.network_address)
+        self.mask = int(subnet.prefixlen)
+        self.broadcast = str(subnet.broadcast_address)
+        self.allocated = ()
+
+    def hosts(self):
+        return tuple(self._hosts)
+
+    def allocate(self, ip):
+        if ip in self._hosts:
+            allocated = [i for i in self.allocated]
+            allocated.append(ip)
+            self.allocated = tuple(allocated)
+
+    def unassigned(self):
+        return tuple(set(self._hosts) - set(self.allocated))
+
+
+if __name__ == '__main__':
+    net1 = IPv4Network('10.1.1.0/29')
+    net2 = IPv4Network('100.7.1.0/26')
+
+    print('net1')
+    print(net1.address)
+    print(net1.hosts())
+    print(net1.broadcast)
+
+    print(net1.allocated)
+    net1.allocate('10.1.1.4')
+    net1.allocate('10.1.1.6')
+    print(net1.allocated)
+
+    print(net1.unassigned())
+
+    print('\nnet2')
+    print(len(net2.hosts()))
+    print(net2.broadcast)
+
+    del net1
